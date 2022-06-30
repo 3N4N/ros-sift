e345d9ac67f69b953567271013a6bd4034454954
Enan Ajmain <3nan.ajmain@gmail.com>
2022-06-30 14:15:41 +0600

    Initial commit

---
+++ src/sifter/src/sifter.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <ros/package.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+
+using namespace std;
+
+int main()
+{
+  string fp_inimg = ros::package::getPath("sifter") + "/assets/input.jpg";
+  string fp_outimg = ros::package::getPath("sifter") + "/assets/output.jpg";
+  cout << fp_inimg << "\n";
+
+  cout << "OpenCV Version: " << CV_VERSION << "\n";
+  const cv::Mat input = cv::imread(fp_inimg, cv::IMREAD_GRAYSCALE);
+
+  cv::Ptr<cv::SIFT> siftPtr = cv::SIFT::create();
+  std::vector<cv::KeyPoint> keypoints;
+  siftPtr->detect(input, keypoints);
+
+  cv::Mat output;
+  cv::drawKeypoints(input, keypoints, output);
+  cv::imwrite(fp_outimg, output);
+
+  return 0;
+}
